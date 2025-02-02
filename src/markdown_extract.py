ebcada967521d0931debc2c7954902397ffbd229
Narsil92 <pblus23@gmail.com>
2025-02-02 16:14:19 +0100

    created markdown_extract.py file with function that capturing images and links into tuples

---
+++ src/markdown_extract.py
@@ -0,0 +1,17 @@
+import re
+
+
+
+def extract_markdown_images(text):   
+    pattern = r"!\[([^\]]+)]\(([^)]+)\)" # RegExp pattern that extracting [alt_text] and (url) of image in markdown into tuple              
+    matches = re.findall(pattern, text) # Creating a tuples of [alt_text] and (url) that's matching the pattern
+    return matches
+
+
+
+def extract_markdown_links(text):
+    pattern= r"\[([^\]]+)]\(([^)]+)\)" # RegExp pattern that extracting [link_text] and (url) of image in markdown into tuple 
+    matches = re.findall(pattern, text) # Creating a tuples of [alt_text] and (url) that's matching the pattern
+    return matches
+
+
